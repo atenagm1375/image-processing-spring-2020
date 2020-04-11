4268acff1a189fd0c1d2decd8a7625f7decbc2ee
atenagm <ashenatena@gmail.com>
2020-04-11 17:58:11 +0430

    add some part of analysis of picture 1 and make men in images 11 and 12 younger

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -36,10 +36,58 @@ def idft(img):
     return filtered_img
 
 
+def fspecial(shape=(3, 3), sigma=0.5):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m + 1, -n:n + 1]
+    h = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+
+
+def make_younger(img, sigma):
+    transformed_img, magnitude_spectrum = dft(img)
+    plot_before_after(img, magnitude_spectrum, True)
+    h = cv2.normalize(fspecial(img.shape, sigma=sigma),
+                      None, 0, 1, cv2.NORM_MINMAX)
+    transformed_img = transformed_img * h
+    filtered_img = idft(transformed_img)
+    return filtered_img
+
+
 def exe1():
     """
         Remove noise from images 1 to 4 with proper filters.
     """
+    img1 = cv2.imread('1.jpg', 0)
+    shifted_transformed_img1, magnitude_spectrum = dft(img1)
+
+    plot_before_after(img1, magnitude_spectrum, True)
+
+    w, h = img1.shape
+    mask = np.ones(img1.shape, dtype=np.uint8)
+    for i in range(10, w, 45):
+        for j in range(20, h, 70):
+            mask[i:i + 15, j:j + 20] = 0
+    # mask[34 - 5:34 + 5, 66 - 5:66 + 5] = 0
+    # mask[34 - 5:34 + 5, 42 - 5:42 + 5] = 0
+    # mask[34 - 5:34 + 5, h // 2 - 5:h // 2 + 5] = 0
+    # mask[98 - 5:98 + 5, 66 - 5:66 + 5] = 0
+    # mask[98 - 5:98 + 5, 42 - 5:42 + 5] = 0
+    # mask[159 - 5:159 + 5, 66 - 5:66 + 5] = 0
+    # mask[159 - 5:159 + 5, 42 - 5:42 + 5] = 0
+    # mask[159 - 5:159 + 5, h // 2 - 5:h // 2 + 5] = 0
+    # mask[34 - 5:34 + 5, 192 - 5:192 + 5] = 0
+    # mask[34 - 5:34 + 5, 218 - 5:218 + 5] = 0
+    # mask[98 - 5:98 + 5, 192 - 5:192 + 5] = 0
+    # mask[98 - 5:98 + 5, 218 - 5:218 + 5] = 0
+    # mask[159 - 5:159 + 5, 192 - 5:192 + 5] = 0
+    # mask[159 - 5:159 + 5, 218 - 5:218 + 5] = 0
+    plt.imshow(magnitude_spectrum * mask, cmap='gray')
+    plt.show()
+
     masked_fourier = shifted_transformed_img1 * mask
     filtered_img1 = idft(masked_fourier)
     mean_filter = (1 / 4) * np.ones((2, 2), dtype=np.uint8)
@@ -90,7 +138,13 @@ def exe3():
     """
         Make images 11 and 12 younger with frequency domain operators.
     """
-    pass
+    img = cv2.imread("11.jpg", 0)
+    younger_img = make_younger(img, 40)
+    plot_before_after(img, younger_img)
+
+    img = cv2.imread("12.jpg", 0)
+    younger_img = make_younger(img, 32)
+    plot_before_after(img, younger_img)
 
 
 def __main__(num):
